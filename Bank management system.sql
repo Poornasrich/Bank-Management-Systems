5679f21c069a7996bbc618d530e5e9a7bb1638c0
Poornasri Ch <114277400+Poornasrich@users.noreply.github.com>
2023-04-11 12:14:32 +0530

    Add files via upload

---
+++ Bank management system.sql
@@ -0,0 +1,62 @@
+--creating tables
+CREATE TABLE UserLogins
+(
+	UserLoginID SMALLINT NOT NULL IDENTITY(1,1),
+	UserLogin VARCHAR(50) NOT NULL,
+	UserPassword VARCHAR(20) NOT NULL,
+	CONSTRAINT pk_UL_UserLoginID PRIMARY KEY(UserLoginID)
+);
+
+
+--Account type 
+CREATE TABLE AccountType
+(
+	AccountTypeID TINYINT NOT NULL IDENTITY(1,1),
+	AccountTypeDescription VARCHAR(30) NOT NULL,
+	CONSTRAINT pk_AT_AccountTypeID PRIMARY KEY(AccountTypeID)
+);
+
+--Creating table named AccountStatusType
+CREATE TABLE AccountStatusType
+(
+	AccountStatusTypeID TINYINT NOT NULL IDENTITY(1,1),
+	AccountStatusTypeDescription VARCHAR(30) NOT NULL,
+	CONSTRAINT pk_AST_AccountStatusTypeID PRIMARY KEY(AccountStatusTypeID)
+);
+
+--creating table named transactiontype
+CREATE TABLE TransactionType
+(
+	TransactionTypeID TINYINT NOT NULL IDENTITY(1,1),
+	TransactionTypeName CHAR(10) NOT NULL,
+	TransactionTypeDescription VARCHAR(50),
+	TransactionFeeAmount SMALLMONEY,
+	CONSTRAINT pk_TT_TransactionTypeID PRIMARY KEY(TransactionTypeID)
+);
+
+--Insert at least 5 rows in each table. 
+/** INSERT rows section **/
+
+insert into UserLogins values('User1', 'Pass1');
+insert into UserLogins values('User2', 'Pass2');
+insert into UserLogins values('User3', 'Pass3');
+insert into UserLogins values('User4', 'Pass4');
+insert into UserLogins values('User5', 'Pass5');
+
+Insert into AccountType values('Savings');
+Insert into AccountType values('Checking');
+
+
+insert into AccountStatusType values('Closed');
+insert into AccountStatusType values('Active');
+
+
+insert into TransactionType values('Balance', 'See money', '0');
+insert into TransactionType values('Transfer', 'Send money', '4500');
+insert into TransactionType values('Receive', 'Get money', '3000');
+insert into TransactionType values('Paid', 'Paid to John', '45000');
+insert into TransactionType values('Statement', 'Checked monthly transaction', '1400');
+
+
+  SELECT * 
+FROM UserLogins,AccountType,AccountStatusType, TransactionType;
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# Bank-Management-Systems
+
+# BANK MANAGEMENT SYSTEMS USING SQL
+
+The project is related to queries in SQL of Bank Management systems.
+
+## Keywords
+
+create, insert, alter, select, groupby, joins
+## 🚀 About Me
+
+I'm a Certified Data Scientist and MBA Graduate, worked on different projects Like Machine Learning, Deep Learning, NLP, PowerBI, Tableau Dashboards.
+
+
+
+## 🛠 Skills
+
+Python, SQL, PowerBI, Tableau, machine learning, deep learning, NLP.
+
+
+
+## Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://poornasri1619.wixsite.com/my-site-1/portfolio)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/poornasri-chervughattu-6a852923b/)
+
+
+
+## Support
+
+Email: poornasri.1619@gmail.com
+
